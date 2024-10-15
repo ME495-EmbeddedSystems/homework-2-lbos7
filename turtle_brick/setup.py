439768a40b28c943afb39fb2ca94710e9d4927cc
Logan Boswell <loganstuartboswell@gmail.com>
2024-10-15 14:27:41 -0500

    Created launch show_turtle.launch.xml and made a cube show up in rviz

--- turtle_brick/setup.py
+++ turtle_brick/setup.py
@@ -4,19 +4,22 @@ package_name = 'turtle_brick'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml',
+                                   'launch/show_turtle.launch.xml',
+                                   'urdf/turtle.urdf.xacro',
+                                   'config/view_robot.rviz']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='lbos7',
     maintainer_email='loganstuartboswell@gmail.com',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='GPLv3',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
