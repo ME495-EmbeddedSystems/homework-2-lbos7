87d4a1eca7b00ad2ff2f2696399b9f5edf24f07c
Logan Boswell <loganstuartboswell@gmail.com>
2024-11-01 16:02:07 -0500

    Made changes to test_brick_launch.py

--- turtle_brick/test/test_brick_launch.py
+++ turtle_brick/test/test_brick_launch.py
@@ -1,67 +1,70 @@
-# import rclpy
-# from rclpy.node import Node
-# import unittest
-# import pytest
-# from launch import LaunchDescription
-# from launch_testing.actions import ReadyToTest
-# from turtle_brick import turtle_robot
-# # Mark the launch description generation as a rostest
-# # Also, it's called generate_test_description() for a test
-# # But it still returns a LaunchDescription
-# @pytest.mark.rostest
-# def generate_test_description():
-#     # Create a node, as usual. But it's useful to keep it around
-#     node = Node(executable=turtle_robot)
-#     # return a tuple (LaunchDescription, extra_dict)
-#     # extra_dict is a dictionary of parameter names -> values that get passed to
-#     # test cases.
-#     return (
-#         LaunchDescription([
-#             node, # here is where you put all launch actions you want to run during the test
-#                   # To test a particular launch file, include it
-#             ReadyToTest() # this is the last action. Can be used elsewhere somehow
-#             ]),
-#             {'myaction': node } # this is a way to pass the node action to the test case
-#             )
-# # The above returns the launch description. Now it's time for the test
-# # The goal is essentially to create a node that can then be used in all tests to
-# # call services and subscribe/publish messages
-# # unlike a regular node, it is often useful to not subclass node but rather
-# # just create it. It is also useful (and necessary) to spin_once() as needed
-# class TestFrequency(unittest.TestCase):
-#     @classmethod
-#     def setUpClass(cls):
-#         """Runs one time, when the testcase is loaded"""
-#         rclpy.init()
+import rclpy
+from rclpy.node import Node
+import unittest
+import pytest
+from launch import LaunchDescription
+from launch_testing.actions import ReadyToTest
+from launch_testing_ros.wait_for_topics import WaitForTopics
+from geometry_msgs.msg import Twist
+# Mark the launch description generation as a rostest
+# Also, it's called generate_test_description() for a test
+# But it still returns a LaunchDescription
+@pytest.mark.rostest
+def generate_test_description():
 
-#     @classmethod
-#     def tearDownClass(cls):
-#         """ Runs one time, when testcase is unloaded"""
-#        rclpy.shutdown()
+    turtle_robot_action = Node(package='turtle_brick',
+                               executable='turtle_robot')
 
-#     def setUp(self):
-#         """Runs before every test"""
-#         # so before every test, we create a new node
-#         self.node = rclpy.create_node('test_node')
+    return (
+        LaunchDescription([
+            turtle_robot_action,
+            ReadyToTest()
+            ]),
+            {'turtle_robot': turtle_robot_action}
+            )
 
-#     def tearDown(self):
-#         """Runs after every test"""
-#         # so after every test we destroy the node
-#         # Is a new node necessary for each test, or could we
-#         # create the nodes when we setupClass?
-#         self.node.destroy_node()
+# The above returns the launch description. Now it's time for the test
+# The goal is essentially to create a node that can then be used in all tests to
+# call services and subscribe/publish messages
+# unlike a regular node, it is often useful to not subclass node but rather
+# just create it. It is also useful (and necessary) to spin_once() as needed
+class TestFrequency(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Runs one time, when the testcase is loaded"""
+        rclpy.init()
 
-#     def test_frequency1(self, launch_service, myaction, proc_output):
-#        """In UnitTest, any function starting with "test" is run as a test
+    @classmethod
+    def tearDownClass(cls):
+        """ Runs one time, when testcase is unloaded"""
+        rclpy.shutdown()
 
-#           Args:
-#              launch_service - information about the launch
-#              myaction - this was passed in when we created the description
-#              proc_output - this object streams the output (if any) from the running process
-#        """
-#        # Here you can use self.node to publish and subscribe
-#        # launch_testing.WaitForTopics waits for something to be published on a topic
-#        # spin with rclpy.spin_once()
-#        # You can check and verify output with proc_output. launch/launch_testing has more information
-#        # You should specify a timeout for rclpy.spin_once() because if no ROS events happen it will
-#        # block forever (but if no events happen that is a bug and the test should fail
+    def setUp(self):
+        """Runs before every test"""
+        self.node = rclpy.create_node('test_node')
+
+    def tearDown(self):
+        """Runs after every test"""
+        self.node.destroy_node()
+
+    def test_frequency(self, launch_service, node, proc_output):
+        """In UnitTest, any function starting with "test" is run as a test
+
+          Args:
+              launch_service - information about the launch
+              myaction - this was passed in when we created the description
+              proc_output - this object streams the output (if any) from the running process
+        """
+        wait_for_topics = WaitForTopics([('turtle1/cmd_vel', Twist)], timeout=5.0)
+        assert wait_for_topics.wait()
+        self.freqs = []
+        self.timestamps = []
+        while len(self.timestamps) < 101:
+            rclpy.spin_once(node)
+            time = self.node.get_clock().now().to_msg()
+            if len(self.timestamps) > 0:
+                self.freqs.append(1/(time - self.timestamps[-1]))
+            self.timestamps.append(time)
+
+        self.assertEqual(sum(self.freqs)/len(self.freqs) < 105)
+        self.assertEqual(sum(self.freqs)/len(self.freqs) > 95)
